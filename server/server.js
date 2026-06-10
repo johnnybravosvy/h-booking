d9a5b32722c5bc54440fff56743c5d03e68d0a32
johnnybravo <ezeufohchibuzor@gmail.com>
2026-06-10 15:15:47 +0100

    production error

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ if (process.env.NODE_ENV === "production") {
   const filePath = path.resolve(__dirname, "build", "index.html");
   app.use(express.static(publicpath));
 
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     return res.sendFile(filePath);
   });
 }
